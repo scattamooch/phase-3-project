787e7b2e400c06962b320d6396f7f9c1da9de6ba
scattamooch <seanstevens729@gmail.com>
2023-07-18 12:06:51 -0400

    Tables work, kinda

--- lib/exports.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///tbdnd.db')
-
-Base = declarative_base()
-
