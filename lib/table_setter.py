552b2812cc3a691172aa89ceeca520aa1675a752
Nolan Rummel <nolan.rummel22@gmail.com>
2023-07-18 16:05:23 -0400

    cleanup redudant code

--- lib/table_setter.py
+++ lib/table_setter.py
@@ -1,8 +1,10 @@
 from models import session, Race, Character, CharClass  
 
+#Deletes Past Session Races/Classes
 session.query( Race ).delete()
-session.commit()
+session.query( CharClass ).delete()
 
+#All Starting Races
 dragonborn = Race(name = "Dragonborn", age = 80, size = "Medium", language = "Common, Draconic")
 human = Race(name = "Human", age = 100, size = "Medium", language = "Common")
 dwarf = Race(name = "Dwarf", age = 350, size = "Medium", language = "Common, Dwarvish")
@@ -13,12 +15,7 @@ halfling = Race(name = "Halfling", age = 250, size = "Small", language = "Common
 half_orc = Race(name = "Half-Orc", age = 75, size = "Medium", language = "Common, Orc")
 tiefling = Race(name = "Tiefling", age = 100, size = "Medium", language = "Common, Infernal")
 
-session.bulk_save_objects([dragonborn, human, dwarf, elf, gnome, half_elf, halfling, half_orc, tiefling])
-session.commit()
-
-session.query( CharClass ).delete()
-session.commit()
-
+#All Starting Classes
 barbarian = CharClass(name = "Barbarian", armor = "light armor, shields", weapons = "simple weapons, martial weapons", starting_gear = "greataxe")
 bard = CharClass(name = "Bard", armor = "light armor", weapons = "simple weapons", starting_gear = "dagger")
 cleric = CharClass(name = "Cleric", armor = "light armor, shields", weapons = "simple weapons", starting_gear = "mace")
@@ -32,53 +29,56 @@ sorcerer = CharClass(name = "Sorcerer", armor = "none", weapons = "daggers, ligh
 warlock = CharClass(name = "Warlock", armor = "light armor", weapons = "simple weapons", starting_gear = "light crossbow, arcane focus")
 wizard = CharClass(name = "Wizard", armor = "none", weapons = "daggers, light crossbows", starting_gear = "dagger, arcane focus")
 
+#Render Starting Tables
+session.bulk_save_objects([dragonborn, human, dwarf, elf, gnome, half_elf, halfling, half_orc, tiefling])
 session.bulk_save_objects([barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, wizard])
+
+#Delete Past Session Characters
+session.query(Character).delete()
+
 session.commit()
 
-# session.query(Character).delete()
-# session.commit()
+#CLI input for new Race
+# user_input = 'not x'
 
+# while user_input != 'x':
+#     user_input = input( '<3 ' )
+#     if user_input == 'r':
 
-user_input = 'not x'
+#         for race in session.query( Race ).all():
+#             print( f'    {race.name}' )
 
-while user_input != 'x':
-    user_input = input( '<3 ' )
-    if user_input == 'r':
-
-        for race in session.query( Race ).all():
-            print( f'    {race.name}' )
-
-    elif user_input == "add race":
-        race_name = input("Enter the name of a race you'd like to add: ")
-        new_race = Race(name = race_name)
-        session.add(new_race)
-        session.commit()
-        print( f"{new_race.name} has been added.")
+#     elif user_input == "add race":
+#         race_name = input("Enter the name of a race you'd like to add: ")
+#         new_race = Race(name = race_name)
+#         session.add(new_race)
+#         session.commit()
+#         print( f"{new_race.name} has been added.")
     
-    elif user_input != 'x':
-        print( f'{user_input} is not a valid option' )
+#     elif user_input != 'x':
+#         print( f'{user_input} is not a valid option' )
 
-print( 'byebye!')
+# print( 'byebye!')
 
 
+#CLI input for new Class
+# user_input = 'not x'
 
-user_input = 'not x'
+# while user_input != 'x':
+#     user_input = input( '<3 ' )
+#     if user_input == 'c':
 
-while user_input != 'x':
-    user_input = input( '<3 ' )
-    if user_input == 'c':
+#         for charclass in session.query( CharClass ).all():
+#             print( f'    {charclass.name}' )
 
-        for charclass in session.query( CharClass ).all():
-            print( f'    {charclass.name}' )
-
-    elif user_input == "add class":
-        charclass_name = input("Enter the name of a race you'd like to add: ")
-        new_class = CharClass(name = charclass_name)
-        session.add(new_class)
-        session.commit()
-        print( f"{new_class.name} has been added.")
+#     elif user_input == "add class":
+#         charclass_name = input("Enter the name of a race you'd like to add: ")
+#         new_class = CharClass(name = charclass_name)
+#         session.add(new_class)
+#         session.commit()
+#         print( f"{new_class.name} has been added.")
     
-    elif user_input != 'x':
-        print( f'{user_input} is not a valid option' )
+#     elif user_input != 'x':
+#         print( f'{user_input} is not a valid option' )
 
-print( 'byebye!')
+# print( 'byebye!')
